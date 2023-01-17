671c26ebc33eed23c2cef063a9bd67d0fe6c605d
TineoC <christophertineo02@gmail.com>
2023-01-17 10:50:48 -0400

    create transaction model, controller and tests

---
+++ ConsoleMoneyTracker/src/main/controller/TransactionController.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleMoneyTracker.src.main.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transaction = ConsoleMoneyTracker.src.main.model.Transaction;
+
+namespace ConsoleMoneyTracker.src.main.controller
+{
+    public class TransactionController
+    {
+        public IList<Transaction>? Transactions;
+
+        public IList<Transaction> GetTransactions()
+        {
+            throw null;
+        }
+    }
+}
---
+++ ConsoleMoneyTracker/src/main/model/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMoneyTracker.src.main.model
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+    }
+}
