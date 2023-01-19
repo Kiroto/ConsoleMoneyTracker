1a4d7931d48bb8cdfdf529d359fbe3ba8a09e5c7
TineoC <christophertineo02@gmail.com>
2023-01-19 09:01:27 -0500

    get accounts method

--- ConsoleMoneyTrackerTests/src/main/controller/AccountControllerTests.cs
+++ ConsoleMoneyTrackerTests/src/main/controller/AccountControllerTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleMoneyTracker.src.main.model;
 
 namespace ConsoleMoneyTracker.src.main.controller.Tests
 {
@@ -12,9 +13,23 @@ namespace ConsoleMoneyTracker.src.main.controller.Tests
     public class AccountControllerTests
     {
         [TestMethod()]
-        public void GetAccountsTest()
+        public void GetAccounts()
         {
-            Assert.Fail();
+            // From some accounts
+            var dopListItem = new ListItem(1, "Dominican Peso", "DOP", "Dominican peso currency");
+
+            var dominicanPeso = new Currency(1, dopListItem, "", 51.2);
+
+            var listItem1 = new ListItem(1, "Cuenta 1", "C-01", "Cuenta de ahorros 1");
+            var listItem2 = new ListItem(2, "Cuenta 2", "C-02", "Cuenta de ahorros 2");
+
+            var account1 = new Account(1, listItem1, dominicanPeso, 320);
+            var account2 = new Account(2, listItem2, dominicanPeso, 1250.63);
+
+            // Return an array of Account
+            var accountController = new AccountController(account1, account2);
+
+            Assert.AreEqual(accountController.GetAccounts(), new Account[] {account1, account2});
         }
     }
 }
